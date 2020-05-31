75deaebfc012ecc13794f85efe7f32d018e16107
ioedumac <eacevedof@yahoo.es>
2020-05-31 21:16:20 +0200

    creo gestion de logs

---
+++ php/lib/trait_log.php
@@ -0,0 +1,36 @@
+<?php
+namespace Theframework\Traits;
+use Theframework\Components\ComponentLogLog As L;
+
+trait TraitLog
+{
+    protected function log($mxVar,$sTitle=NULL)
+    {
+        $pathlogs = realpath(__DIR__."/../../logs");
+        $oLog = new L("sql",$pathlogs);
+        $oLog->save($mxVar,$sTitle);
+    }
+
+    protected function logd($mxVar,$sTitle=NULL)
+    {
+        $pathlogs = realpath(__DIR__."/../../logs");
+        $oLog = new L("debug",$pathlogs);
+        $oLog->save($mxVar,$sTitle);
+    }
+
+    protected function logpost()
+    {
+        $this->logd($_POST,"POST");
+    }
+
+    protected function logget()
+    {
+        $this->logd($_GET,"GET");
+    }
+
+    protected function logreq()
+    {
+        $this->logd($_REQUEST,"REQUEST");
+    }
+
+}//AppLogTrait
