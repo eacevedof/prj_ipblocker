0c048c3810a53771c8778628c6670be28eddddc4
ioedumac <eacevedof@yahoo.es>
2020-05-31 20:40:11 +0200

    subo sin trazas

--- php/public/ipblocker.php
+++ php/public/ipblocker.php
@@ -1,7 +1,7 @@
 <?php
 //ipblocker.php
 $pathlib = realpath(__DIR__."/../lib");
-die("pathlib:$pathlib");
+//die("pathlib:$pathlib");
 include("$pathlib/functions.php");
 include("$pathlib/helper_request.php");
 include("$pathlib/component_config.php");
