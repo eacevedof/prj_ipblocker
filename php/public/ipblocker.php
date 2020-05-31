19a9dc4a7dc0b1f4e73d3b63bb2b9831c2801375
ioedumac <eacevedof@yahoo.es>
2020-05-31 20:38:50 +0200

    subo con trazas

--- php/public/ipblocker.php
+++ php/public/ipblocker.php
@@ -1,12 +1,14 @@
 <?php
 //ipblocker.php
-include("../lib/functions.php");
-include("../lib/helper_request.php");
-include("../lib/component_config.php");
-include("../lib/component_mysql.php");
-include("../lib/component_mailing.php");
-include("../lib/provider_base.php");
-include("../lib/component_ipblocker.php");
+$pathlib = realpath(__DIR__."/../lib");
+die("pathlib:$pathlib");
+include("$pathlib/functions.php");
+include("$pathlib/helper_request.php");
+include("$pathlib/component_config.php");
+include("$pathlib/component_mysql.php");
+include("$pathlib/component_mailing.php");
+include("$pathlib/provider_base.php");
+include("$pathlib/component_ipblocker.php");
 
 $o = new \TheFramework\Components\ComponentIpblocker();
 $o->handle_request();
