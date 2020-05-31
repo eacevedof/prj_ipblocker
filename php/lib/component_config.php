99f0b047a6f11eaa8e5e838985b699753150c3ef
ioedumac <eacevedof@yahoo.es>
2020-05-31 14:33:17 +0200

    php todo ok

---
+++ php/lib/component_config.php
@@ -0,0 +1,32 @@
+<?php
+namespace TheFramework\Components;
+
+class ComponentConfig
+{
+    private CONST pathcfg = "../config/dbconfig.json";
+
+    public static function get_schema($id,$database)
+    {
+        $content = file_get_contents(self::pathcfg);
+        $array = json_decode($content);
+        foreach ($array as $arcfg)
+        {
+            if($arcfg["id"] == $id) {
+                $schemas = $arcfg["schemas"];
+                foreach ($schemas as $arschm)
+                {
+                    if($arschm["database"] == $database)
+                    {
+                        return [
+                            "server" => $arcfg["server"],
+                            "database" => $database,
+                            "user"=>$arcfg["user"],
+                            "password"=>$arcfg["password"],
+                        ];
+                    }
+                }
+            }
+        }
+        return [];
+    }
+}
--- php/public/ipblocker.php
+++ php/public/ipblocker.php
@@ -1,12 +1,10 @@
 <?php
 //ipblocker.php
 include("../lib/functions.php");
-include("../lib/component_mysql.php");
-include("../lib/component_mailing.php");
-include("../lib/provider_base.php");
 include("../lib/component_ipblocker.php");
 
 
 $o = new \TheFramework\Components\ComponentIpblocker();
 $o->handle_request();
 
+echo "ipblocker.php";
