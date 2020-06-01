2af7ec0218e1d88163c4080622b7f9a756b013e9
ioedumac <eacevedof@yahoo.es>
2020-06-01 15:18:03 +0200

    me da error de modulo pero funciona

--- vuejs/src/components/TodoForm.vue
+++ vuejs/src/components/TodoForm.vue
@@ -1,7 +1,12 @@
 <template>
-  <div>
-    TodoForm
-  </div>
+  <form>
+    <div class="form-group">
+      <label for="input-todo">Email address</label>
+      <input type="text" class="form-control" id="input-todo" aria-describedby="emailHelp" placeholder="Enter email">
+      <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+    </div>
+    <button type="submit" class="btn btn-primary">Submit</button>
+  </form>
 </template>
 
 <script lang="ts">
