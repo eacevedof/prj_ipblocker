51dd1481eedede013ccd69ec52f3e8582efdcc0a
ioedumac <eacevedof@yahoo.es>
2020-06-01 15:01:47 +0200

    vue: todoform incluido

---
+++ vuejs/src/components/TodoForm.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    TodoForm
+  </div>
+</template>
+
+<script lang="ts">
+import {Component, Vue} from "vue-property-decorator";
+
+
+export default class TodoForm extends Vue{
+  
+}
+</script>
