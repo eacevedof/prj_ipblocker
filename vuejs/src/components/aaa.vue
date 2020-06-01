e0003f64796c19da9c3b92ef9f33f2259eeb785f
ioedumac <eacevedof@yahoo.es>
2020-06-01 14:50:00 +0200

    todolist

---
+++ vuejs/src/components/aaa.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    aaa
+  </div>
+</template>
+
+<script lang="ts">
+import {Component, Vue} from "vue-property-decorator";
+@Component
+export default class TodoApp extends Vue{
+  
+}
+</script>
---
+++ vuejs/src/views/TodoApp.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    todo app
+  </div>
+</template>
+
+<script lang="ts">
+import {Component, Vue} from "vue-property-decorator";
+@Component
+export default class TodoApp extends Vue{
+  
+}
+
+</script>
