2928a00a8b60fb0b21c779b77f948e260f1aa150
ioedumac <eacevedof@yahoo.es>
2020-06-01 14:50:00 +0200

    todolist

---
+++ vuejs/src/components/TdodList.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+    <table class="table">
+      <thead>
+        <th>id</th>
+        <th>todo</th>
+        <th>eliminar</th>
+      </thead>
+
+      <tbody v-if="todos.length > 0">
+        <tr v-for="{todo, index} in todos" :key="index">
+          <td>{{ index + 1 }}</td>
+          <td>{{ todo }}</td>
+          <td>
+            <button @click="fn_removetodo(index)" class="btn btn-danger btn-block">
+              Eliminar
+            </button>
+          </td>
+        </tr>
+      </tbody>
+
+      <tbody v-else>
+        <tr>
+          <td colspan="3">No todos</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script lang="ts">
+import {Component, Vue} from "vue-property-decorator";
+
+@Component
+export default class TodoApp extends Vue{
+  
+  todos: Array<string> = [];
+  
+  fn_newtodo(todo){
+    this.todos.push(todo)
+  }
+
+  fn_removetodo(index){
+    this.todos.splice(index,1)
+  }
+
+  fn_removetodos(){
+    this.todos = []
+  }
+
+}
+
+</script>
--- vuejs/src/router/index.ts
+++ vuejs/src/router/index.ts
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter, { RouteConfig } from 'vue-router'
 import Home from '../views/Home.vue'
+import TodoApp from '../views/TodoApp.vue'
 
 Vue.use(VueRouter)
 
@@ -17,7 +18,15 @@ Vue.use(VueRouter)
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+  },
+  {
+    path: '/todo-app',
+    name: 'todo-app',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: TodoApp
+  }  
 ]
 
 const router = new VueRouter({
