0b2aa6317f02629fd64899432c02b84bda81347e
ioedumac <eacevedof@yahoo.es>
2020-06-02 14:23:31 +0200

    configurando grid

--- vuejs/pannel/src/views/Grid.vue
+++ vuejs/pannel/src/views/Grid.vue
@@ -1,5 +1,12 @@
 <template>
-  <div>
-    <h1>Hola Grid</h1>
-  </div>
+  <vi-container>
+    <v-row>
+      <v-col cols="12">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Id, ut excepturi? Aperiam obcaecati id blanditiis culpa maiores tempore dicta voluptatum quo rerum cum? Et dolore odio quaerat eum optio fuga?
+      </v-col>
+      <v-col cols="12">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem sunt accusantium laborum earum quod doloremque, delectus itaque beatae omnis ipsa culpa ipsum. Reprehenderit odio dignissimos architecto quis optio omnis quod!
+      </v-col>
+    </v-row>
+  </vi-container>
 </template>
