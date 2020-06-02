002177d53766661fbf7b0e90da82fe8a3bcba819
ioedumac <eacevedof@yahoo.es>
2020-06-02 14:07:39 +0200

    he instalado vuetify

---
+++ vuejs/pannel/src/components/Aaa.vue
@@ -0,0 +1,19 @@
+<template>
+  <h1>{{ name }}</h1>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  name: 'Aaaa',
+  
+
+  props: {
+    msg: String,
+  },
+});
+</script>
+
+<style>
+</style>
