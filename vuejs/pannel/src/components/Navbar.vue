773ffa8533b935add392913253c8190fd7e33ded
ioedumac <eacevedof@yahoo.es>
2020-06-02 15:42:34 +0200

    con v-btn y router

--- vuejs/pannel/src/components/Navbar.vue
+++ vuejs/pannel/src/components/Navbar.vue
@@ -4,36 +4,21 @@
     color="primary"
     dark
   >
-    <div class="d-flex align-center">
-      <v-img
-        alt="Vuetify Logo"
-        class="shrink mr-2"
-        contain
-        src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-        transition="scale-transition"
-        width="40"
-      />
+  
+  <v-btn class="btn-edit"
+      color="blue"
+      :to="{ name: 'Home',}">
+      Home
+  </v-btn>
 
-      <v-img
-        alt="Vuetify Name"
-        class="shrink mt-1 hidden-sm-and-down"
-        contain
-        min-width="100"
-        src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-        width="100"
-      />
-    </div>
+  <v-btn class="btn-edit"
+      color="blue"
+      :to="{ name: 'Login',}">
+      Login
+  </v-btn>  
+  
 
-    <v-spacer></v-spacer>
-
-    <v-btn
-      href="https://github.com/vuetifyjs/vuetify/releases/latest"
-      target="_blank"
-      text
-    >
-      <span class="mr-2">Latest Release</span>
-      <v-icon>mdi-open-in-new</v-icon>
-    </v-btn>
+    
   </v-app-bar>
 </template>
 
