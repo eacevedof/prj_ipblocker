ba77f199ef97dfea9dd59323f86db4a8e8f3d199
ioedumac <eacevedof@yahoo.es>
2020-06-05 21:26:35 +0200

    vue - ya se muestra con el store

---
+++ vuejs/pannel/src/components/navigation/Navbar.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-toolbar app class="primary">
+
+    <v-app-bar-nav-icon></v-app-bar-nav-icon>
+
+    <v-toolbar-title >IP Blocker {{$store.state.globalx}}</v-toolbar-title>
+    
+    <v-spacer />
+    <v-btn class="success">
+      login
+    </v-btn>
+    <v-btn class="error">
+      salir
+    </v-btn>
+
+  </v-toolbar>
+
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+//import Hamburguer from "@/components/navigation/Hamburguer.vue"
+
+export default Vue.extend({
+
+  name: "Navbar",  
+
+  components: {
+    //Hamburguer,
+  },
+
+  data () {
+    return {
+      
+    }
+  },
+})
+</script>
--- vuejs/pannel/src/store/index.ts
+++ vuejs/pannel/src/store/index.ts
@@ -5,6 +5,8 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
+    navhamburguer: false,
+    globalx: "Soy la global",
   },
   mutations: {
   },
