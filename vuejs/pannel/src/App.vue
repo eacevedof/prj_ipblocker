6b41210cefd7041eb1b09b0116fec7e11b1e92e0
ioedumac <eacevedof@yahoo.es>
2020-06-05 21:26:35 +0200

    vue - ya se muestra con el store

--- vuejs/pannel/src/App.vue
+++ vuejs/pannel/src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <v-app>
     <navbar />
+    <hamburguer />
     <v-content>
       <router-view />
     </v-content>
@@ -9,13 +10,15 @@
 
 <script lang="ts">
 import Vue from 'vue';
-import Navbar from "@/components/Navbar.vue"
+import Navbar from "@/components/navigation/Navbar.vue"
+import hamburguer from "@/components/navigation/Hamburguer.vue"
 
 export default Vue.extend({
   name: 'App',
 
   components: {
-    Navbar
+    Navbar,
+    hamburguer
   },
 
   data: () => ({
