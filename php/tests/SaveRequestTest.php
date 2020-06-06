3da377c1864082a5e791b6a40e5d1d94d763b59d
ioedumac <eacevedof@yahoo.es>
2020-06-06 12:13:49 +0200

    no pasa el test

--- php/tests/SaveRequestTest.php
+++ php/tests/SaveRequestTest.php
@@ -3,29 +3,35 @@ include("BaseTest.php");
 use Tests\BaseTest;
 use \TheFramework\Components\ComponentIpblocker;
 
+
 final class SaveRequestTest extends BaseTest
 {
 
-    public function __construct()
-    {
-        $_POST = [];
-        $_FILES = [];
-        $_GET = [];
-    }
-
-
     private function pr()
     {
-        print_r($_POST);
-        print_r($_GET);
-        print_r($_FILES);
+        //print_r($_POST);
+        //print_r($_GET);
+        //print_r($_FILES);
+    }
+
+    private function _execute_ipblocker()
+    {
+        (new ComponentIpblocker())->handle_request();
+    }
+
+    private function _test_blocked_get()
+    {
+        $this->reset_all()->
+            add_get("","=ftp://")
+            ->add_get("content","die(@md5(");
+
+        $this->log_globals();
+        $this->_execute_ipblocker();
     }
 
     public function run()
     {
-        $this->pr();
-        $o = new ComponentIpblocker();
-        $o->handle_request();
+        $this->_test_blocked_get();
     }
 }
 
