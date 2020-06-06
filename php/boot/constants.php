5dbc5ba7ab66639b72898447dbed31cfb380f782
ioedumac <eacevedof@yahoo.es>
2020-06-06 12:34:09 +0200

    php haciendo pruebas

--- php/boot/constants.php
+++ php/boot/constants.php
@@ -1,9 +1,10 @@
 <?php
 //constants.php
-define("IPB_ENABLE_LOGS",0);
+define("IPB_ENABLE_LOGS",1);
 $pathlib = realpath(__DIR__."/../lib");
 $pathconfig = realpath(__DIR__."/../../config");
 $pathlogs = realpath(__DIR__."/../../logs");
+//print_r($pathlogs);die;
 define("IPB_PATH_CONFIG",$pathconfig);
 define("IPB_PATH_LOGS",$pathlogs);
 define("IPB_PATH_LIB",$pathlib);
