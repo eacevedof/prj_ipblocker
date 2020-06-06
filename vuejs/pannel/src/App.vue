f0c7ce12da7e397f0872835c7f63fe4c7edaddf2
ioedumac <eacevedof@yahoo.es>
2020-06-06 13:01:21 +0200

    los tests pasan bien

--- vuejs/pannel/src/App.vue
+++ vuejs/pannel/src/App.vue
@@ -28,5 +28,6 @@ export default Vue.extend({
   data: () => ({
     //
   }),
+  
 });
 </script>
--- vuejs/pannel/src/router/index.ts
+++ vuejs/pannel/src/router/index.ts
@@ -8,6 +8,9 @@ const routes: Array<RouteConfig> = [
     path: '/',
     name: 'home',
     component: Home,
+    meta:{
+      title: "IP Blocker"
+    }
   },
   {
     path: '/login',
@@ -16,6 +19,9 @@ const routes: Array<RouteConfig> = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/Login.vue'),
+    meta:{
+      title: "Login"
+    }
   },
   {
     path: '/grid',
