1923492baa847b6e64d3c29fbbf754560c6635d7
ioedumac <eacevedof@yahoo.es>
2020-06-07 18:12:13 +0200

    antes de lanzar el test

--- php/lib/provider_base.php
+++ php/lib/provider_base.php
@@ -5,20 +5,28 @@ use TheFramework\Components\ComponentSearchbots as sb;
 use TheFramework\Components\ComponentConfig as cfg;
 use TheFramework\Components\Db\ComponentMysql;
 use Theframework\Traits\TraitLog;
+use TheFramework\Helpers\HelperRequest as req;
 
 class ProviderBase
 {
     use TraitLog;
 
+    private $req;
     private $db;
     private $remoteip;
 
-    public function __construct($remoteip)
+    public function __construct()
     {
         $dbname = $this->_get_dbname_by_env();
         $config = cfg::get_schema("c1", $dbname);
         $this->db = new ComponentMysql($config);
-        $this->remoteip = $remoteip;
+        $this->req = req::getInstance();
+        $this->remoteip = $this->req->get_remoteip();
+    }
+
+    private function _get_searchbot()
+    {
+        return sb::get_name($this->remoteip);
     }
 
     private function _get_dbname_by_env()
@@ -27,6 +35,32 @@ class ProviderBase
         if($env=="prod")
             return "dbs433062";
         return "db_security";
+    }
+
+    private function _save_app_ip()
+    {
+        $whois = $this->req->get_whois();
+        $sql = "
+        -- save_app_ip 1
+        INSERT INTO app_ip (remote_ip, country, whois) VALUES('$this->remoteip','{$whois["country"]}','{$whois["whois"]}')";
+
+        $searchbot = $this->_get_searchbot();
+        if($searchbot)
+            $sql = "
+            -- save_app_ip 2
+            INSERT INTO app_ip (remote_ip, country, whois) VALUES('$this->remoteip','{$whois["country"]}','host:$searchbot')";
+
+        $this->db->exec($sql);
+    }
+
+    private function _to_json($arvar)
+    {
+        if($arvar) {
+            $string = json_encode($arvar);
+            $string = str_replace("'","\\'",$string);
+            return $string;
+        }
+        return "";
     }
 
     public function is_blacklisted()
@@ -48,127 +82,23 @@ class ProviderBase
         return $id;
     }
 
-    private function get_searchbot()
-    {
-        return sb::get_name($this->remoteip);
-    }
-
-    private function save_app_ip()
-    {
-        $sql = "
-        -- save_app_ip 
-        INSERT INTO app_ip (remote_ip) VALUES('$this->remoteip')";
-
-        $searchbot = $this->get_searchbot();
-        if($searchbot)
-            $sql = "
-            -- save_app_ip 
-            INSERT INTO app_ip (remote_ip, whois) VALUES('$this->remoteip','host:$searchbot')";
-
-        $this->db->exec($sql);
-    }
-
-    private function to_json($arvar)
-    {
-        if($arvar) {
-            $string = json_encode($arvar);
-            $string = str_replace("'","\\'",$string);
-            return $string;
-        }
-        return "";
-    }
-
     public function save_request()
     {
-        if(!$this->is_registered()) $this->save_app_ip();
+        if(!$this->is_registered()) $this->_save_app_ip();
 
-        $requesturi = addslashes($_SERVER["REQUEST_URI"] ?? "no-req-uri");
-        $domain = $_SERVER['HTTP_HOST'] ?? "no-req-domain";
-        $get = $this->to_json($_GET);
+        $requesturi = addslashes($this->req->get_requri());
+        $domain = $this->req->get_domain();
+        $get = $this->_to_json($_GET);
         $post = $_POST;
         if(isset($post["password"])) $post["password"] = "****";
-        $post = $this->to_json($post);
-        $files = $this->to_json($_FILES);
+        $post = $this->_to_json($post);
+        $files = $this->_to_json($_FILES);
 
         $sql = "
         -- save_request
         INSERT INTO app_ip_request (`remote_ip`,`domain`,`request_uri`,`post`,`get`,`files`) 
         VALUES ('$this->remoteip','$domain','$requesturi','$post','$get','$files')";
         $this->db->exec($sql);
-    }
-
-    private function _is_and($arwords,$strcontent)
-    {
-        foreach ($arwords as $w)
-            if(!strstr($strcontent,$w))
-                return false;
-        return true;
-    }
-
-    private function _is_andkeywords($strcontent,$method)
-    {
-        //print_r($strcontent);die;
-        $keywordsand = cfg::get_keywords("and",$method);
-        foreach($keywordsand as $arkw)
-            if($this->_is_and($arkw,$strcontent))
-                return implode(",",$arkw);
-        return false;
-    }
-
-    private function _is_orkeywords($strcontent,$method)
-    {
-        $keywordsor = cfg::get_keywords("or",$method);
-        //print_r($keywordsor);die;
-        foreach($keywordsor as $kw)
-            if(strstr($strcontent,$kw))
-                return $kw;
-        return false;
-    }
-
-    private function _get_json_ofmethod($method="post")
-    {
-        if($method=="post") {
-            $post = $_POST;
-            unset($post["password"]);
-            $json = $this->to_json($post);
-        }
-        else
-            $json = $this->to_json($_GET);
-        $json = strtolower($json);
-        $json = str_replace("\/","/",$json);
-        return $json;
-    }
-
-    private function _is_method_nok($method="post")
-    {
-        if($method=="post" && !$_POST) return "";
-        if($method=="get" && !$_GET) return "";
-
-        $methodjson = $this->_get_json_ofmethod($method);
-        //if($_POST && $this->_is_unicode($methodjson))
-            //return "unicode";
-
-        $isorkw = $this->_is_orkeywords($methodjson, $method);
-        if($isorkw)
-            return "or {$method}:$isorkw";
-
-        $isandkw = $this->_is_andkeywords($methodjson, $method);
-        if($isandkw)
-            return "and {$method}:$isandkw";
-
-        return "";
-    }
-
-
-    public function get_forbidden_words()
-    {
-
-        //comprueba si todo va bien en post
-        $isnok = $this->_is_method_nok();
-        if($isnok) return $isnok;
-        $isnok = $this->_is_method_nok("get");
-        if($isnok) return $isnok;
-        return "";
     }
 
     public function add_to_blacklist($kws)
