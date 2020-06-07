504412afc3e3cafaa48c8f789fda5633f9679be1
ioedumac <eacevedof@yahoo.es>
2020-06-07 21:43:17 +0200

    php ya pasa _test_blocked_by_post_required

--- php/tests/SaveRequestTest.php
+++ php/tests/SaveRequestTest.php
@@ -8,7 +8,7 @@ final class SaveRequestTest extends BaseTest
     private function _test_non_blocked_post()
     {
         $this->reset_all()
-            ->add_server("REMOTE_ADDR","192.168.1.10")
+            ->add_server("REMOTE_ADDR","192.168.1.1")
             ->add_server("HTTP_HOST","theframework.es")
             ->add_server("REQUEST_URI","/en/contact/")
             ->add_post("hidAction","insert");
@@ -20,7 +20,7 @@ final class SaveRequestTest extends BaseTest
     private function _test_blocked_by_post_required()
     {
         $this->reset_all()
-            ->add_server("REMOTE_ADDR","192.168.1.15")
+            ->add_server("REMOTE_ADDR","192.168.1.2")
             ->add_server("HTTP_HOST","theframework.es")
             ->add_server("REQUEST_URI","/en/contact/")
             ->add_post("user","juan@mail.com")
@@ -28,6 +28,20 @@ final class SaveRequestTest extends BaseTest
 
         $this->log_globals();
         $this->_execute_ipblocker("_test_blocked_by_post_required");
+    }
+
+    private function _test_blocked_by_get_not_null()
+    {
+        $this->reset_all()
+            ->add_server("REMOTE_ADDR","192.168.1.3")
+            ->add_server("HTTP_HOST","theframework.es")
+            ->add_server("REQUEST_URI","/en/contact/")
+            ->add_post("hidAction","insert")
+            ->add_get("some","querystring")
+            ->add_get("other","querystring");
+
+        $this->log_globals();
+        $this->_execute_ipblocker("_test_blocked_by_get_not_null");
     }
 
     private function _test_unicode_blocked_post()
@@ -71,7 +85,8 @@ final class SaveRequestTest extends BaseTest
     public function run()
     {
         //$this->_test_non_blocked_post();
-        $this->_test_blocked_by_post_required();
+        //$this->_test_blocked_by_post_required();
+        $this->_test_blocked_by_get_not_null();
         //$this->_test_unicode_blocked_post();
         //$this->_test_blocked_get();
         //$this->_test_non_blocked_post();
