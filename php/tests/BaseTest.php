f6e86f92bc5273c62c8beff8a1301892ba49d44f
ioedumac <eacevedof@yahoo.es>
2020-06-07 18:40:43 +0200

    php aplico prueba de obligatoriedad de nulos

--- php/tests/BaseTest.php
+++ php/tests/BaseTest.php
@@ -15,7 +15,7 @@ abstract class BaseTest
         $_POST = [];
         $_FILES = [];
         $_GET = [];
-
+        $_SERVER = [];
     }
 
     protected function log_globals()
@@ -23,6 +23,7 @@ abstract class BaseTest
         $this->logd($_POST,"POST");
         $this->logd($_GET,"GET");
         $this->logd($_FILES,"FILES");
+        $this->logd($_SERVER,"SERVER");
     }
 
     protected function add_post($k,$v)
@@ -49,9 +50,15 @@ abstract class BaseTest
         return $this;
     }
 
+    protected function add_server($k,$v)
+    {
+        if(is_string($k)) $_SERVER[$k] = $v;
+        return $this;
+    }
+
     protected function reset_all()
     {
-        $_POST=[]; $_GET=[]; $_FILES=[];
+        $_POST=[]; $_GET=[]; $_FILES=[]; $_SERVER = [];
         return $this;
     }
 
