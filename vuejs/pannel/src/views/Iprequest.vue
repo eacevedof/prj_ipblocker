afc10c0ef173eb9097d0afe86be5ed548b0cd148
ioedumac <eacevedof@yahoo.es>
2020-06-09 20:38:31 +0200

    vue hago el redirct

--- vuejs/pannel/src/views/Iprequest.vue
+++ vuejs/pannel/src/views/Iprequest.vue
@@ -9,24 +9,37 @@
   </v-container>
 </template>
 <script lang="ts">
+import {mapMutations, mapActions, mapState} from "vuex"
 import Scrumbs from "@/components/navigation/Scrumbs.vue"
 import api from "@/providers/api.ts"
 
 export default {
   name: "IpRequest",
-
-  components:{
-    Scrumbs
-  },
-
-  beforemount: async function(){
-    console.log("before")
-  },
   
-
   components: {
     Scrumbs,
   },
+
+  data: () => ({
+    valid: true,
+
+  }),//data
+
+  beforeMount: async function(){
+    //comprobar si se esta logado, si no está redirect a login
+  },
+
+  mounted: async function(){
+    ;
+  },
+
+  computed:{
+    ...mapState(["myip"])
+  },
+
+  methods:{
+    ...mapMutations(["set_sidebar"]),
+  }  
 
 };
 </script>
