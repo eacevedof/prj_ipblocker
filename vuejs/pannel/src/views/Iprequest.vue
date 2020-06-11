9f07d667aa0aa761cf714fe75029926ab67a4df2
ioedumac <eacevedof@yahoo.es>
2020-06-11 16:39:12 +0200

    vue: cambio forma de validacion login

--- vuejs/pannel/src/views/Iprequest.vue
+++ vuejs/pannel/src/views/Iprequest.vue
@@ -3,8 +3,8 @@
   <v-container>
 
     <scrumbs pagename="iprequest" />
-    <list />
     
+    <list />
 
   </v-container>
 </template>
@@ -17,6 +17,7 @@ import Scrumbs from "@/components/navigation/Scrumbs.vue"
 import list from "@/modules/iprequest/list.vue"
 
 export default {
+  
   name: "IpRequest",
   
   components: {
@@ -24,50 +25,20 @@ export default {
     list,
   },
 
-  data: () => ({
-  
-  }),//data
-
-  beforeMount: function(){
-    ;
-    //comprobar si se esta logado, si no está redirect a login
+  beforeMount: async function(){
+    await this.async_islogged()
+    if(!this.islogged)
+      this.$router.push({name:"login"})
   },
-
-  mounted: function(){
-    this.load_data()
-  },
-
-  created(){;},
 
   computed:{
+    //getters
     ...mapState(["islogged"]),
   },
-
-  watch:{},
 
   methods:{
     //setters
     ...mapActions(["async_islogged"]),
-
-    load_data: async function(){
-      await this.async_islogged()
-      if(!this.islogged)
-        this.$router.push({name:"login"})
-
-      console.log("iprequest.mounted.islogged: ",this.islogged)
-      if(this.islogged){
-        const usertoken = db.select("usertoken")
-        if(!usertoken) return
-        const response = await api.async_get_ip_request(usertoken)
-        this.rows = response.result
-        this.foundrows = response.foundrows
-        //alert(this.foundrows)
-        //alert(JSON.stringify(response));
-        //console.log("THIS.ROWS: ",this.foundrows)
-        console.table(this.rows)
-      }
-    },
   }
-
-};
+}
 </script>
