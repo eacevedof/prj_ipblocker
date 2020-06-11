19a9d23f3717acf2572b55ff5e7e53c7ce29a7c5
ioedumac <eacevedof@yahoo.es>
2020-06-11 21:53:51 +0200

    vue: refactor nombres

---
+++ vuejs/pannel/src/views/ipblacklist.vue
@@ -0,0 +1,52 @@
+<!--login.vue-->
+<template>
+  <v-container>
+
+    <scrumbs pagename="ipblacklist" />
+    
+    <list />
+    
+  </v-container>
+</template>
+<script lang="ts">
+import {mapMutations, mapActions, mapState} from "vuex"
+import api from "../providers/api"
+import db from "../helpers/localdb"
+
+import Scrumbs from "@/components/navigation/Scrumbs.vue"
+import list from "@/modules/iprequest/list.vue"
+
+export default {
+
+  name: "ipblacklist",
+  
+  components: {
+    Scrumbs,
+    list,
+  },
+
+  beforeMount: async function (){
+    console.log("ipblacklist beforeMount")
+    await this.async_islogged()
+    if(!this.islogged)
+      this.$router.push({name:"login"})
+  },
+
+  mounted: async function(){
+    console.log("ipblacklist mounted")
+    //await this.async_islogged()
+    //if(!this.islogged)
+      //this.$router.push({name:"login"})
+  },
+
+  computed:{
+    //getters
+    ...mapState(["islogged"]),
+  },
+
+  methods:{
+    //setters
+    ...mapActions(["async_islogged"]),
+  }
+}
+</script>
