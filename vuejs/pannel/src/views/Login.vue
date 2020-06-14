6baddf1cc716a5de6222b203c0a7bb62689d79be
ioedumac <eacevedof@yahoo.es>
2020-06-14 11:19:51 +0200

    vue: cambio login a su módulo

--- vuejs/pannel/src/views/Login.vue
+++ vuejs/pannel/src/views/Login.vue
@@ -3,21 +3,23 @@
   <v-container>
 
     <scrumbs pagename="login" />
+    
     <div class="mt-5"></div>
-    <form-login />
+
+    <formlogin />
   
   </v-container>
 </template>
 <script lang="ts">
 import Scrumbs from "@/components/navigation/Scrumbs.vue"
-import FormLogin from "@/components/forms/FormLogin.vue"
+import formlogin from "@/modules/login/form_login.vue"
 
 export default {
   name: "login",
 
   components: {
     Scrumbs,
-    FormLogin,
+    formlogin,
   },
 
 };
