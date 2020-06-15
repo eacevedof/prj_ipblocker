901790894a2f8a49b8cac3f336311ec2d04f9d36
ioedumac <eacevedof@yahoo.es>
2020-06-15 12:09:06 +0200

    vue: dots submenu no funciona

--- vuejs/pannel/src/components/common/menus/submenu_rudc.vue
+++ vuejs/pannel/src/components/common/menus/submenu_rudc.vue
@@ -1,45 +1,37 @@
 <template>
-  <v-row>
-    <v-col cols="12" sm="6" offset-sm="3">
-      <v-card height="200px">
-        <v-card-title class="blue white--text">
-          <span class="headline">Menu</span>
-
-          <v-spacer></v-spacer>
-
-          <v-menu 
-            v-model="isvisible"
-            bottom left
-          >
-
-
-            <v-list>
-              <v-list-item
-                v-for="(item, i) in items"
-                :key="i"
-                @click="items.evt"
-              >
-                <v-list-item-title>{{ item.title }}</v-list-item-title>
-              </v-list-item>
-            </v-list>
-          </v-menu>
-        </v-card-title>
-
-        <v-card-text>Lorem Ipsum</v-card-text>
-      </v-card>
-    </v-col>
-  </v-row>
+  <div>
+    <v-menu
+      v-model="isvisible"
+      :position-x="x"
+      :position-y="y"
+      absolute
+      offset-y
+    >
+      <v-list>
+        <v-list-item
+          v-for="(item, index) in items"
+          :key="index"
+          @click="item.evt"
+        >
+          <v-list-item-title>{{ item.title }}</v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-menu>
+  </div>
 </template>
 
-<script>
+<script lang="ts">
 export default {
   name: "submenu-rudc",
 
-  props:{
-    isvisible: Boolean,
+  created(){
+    alert("created")
   },
 
   data: () => ({
+    showMenu: false,
+    x:0,
+    y:0,
     items: [
       { title: 'Clone', evt:"clone" },
       { title: 'Update', evt:"update" },
@@ -48,7 +40,21 @@ export default {
     ],
   }),
 
+  watch:{
+    isvisible: function(e){
+      alert("iiissssvvv")
+      e.preventDefault()
+      this.showMenu = false
+      this.x = e.clientX
+      this.y = e.clientY
+      this.$nextTick(() => {
+        this.showMenu = true
+      })
+    }
+  },
+
   methods:{
+   
     clone(){
       alert("clone")
       this.$emit("evtclone")
