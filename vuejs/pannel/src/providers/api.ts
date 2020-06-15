c2a071bbced3fa769d04affe3b07fa5a6c727362
ioedumac <eacevedof@yahoo.es>
2020-06-15 17:26:47 +0200

    vue: form-detail llama a flag

--- vuejs/pannel/src/providers/api.ts
+++ vuejs/pannel/src/providers/api.ts
@@ -257,6 +257,37 @@ const Api = {
     }
   }, //async_delete
 
+  async_getflags: async(arrows) => {
+    if(is_undefined(arrows.length) || arrows.length==0)
+        return
+    
+        //https://restcountries.eu/rest/v2/alpha?codes=ES;PE
+    const url = `https://restcountries.eu/rest/v2/alpha?codes=`
+    //hay que enviar header: apify-auth: token
+    try {
+      const fnunique = (value, i, self) => (self.indexOf(value) == i)
+
+      const arcodes = arrows.map(objrow => objrow.country).filter(fnunique)
+      if(!arcodes) return
+      const strcodes = arcodes.join(";")
+      const response = await axios.get(url+strcodes)
+      
+      //devuelve el num de registros afectados
+      if(is_undefined(response.data))
+        throw new Error("Wrong data received from server. getflags result")
+
+      const fullinfo = response.data
+      const mindata = fullinfo.map(objinfo => ({name:objinfo.name,flag:objinfo.flag}))
+      //pr(mindata,"mindata")
+      return mindata
+    } 
+    catch (e) {
+      console.error("ERROR: api.async_getflags.url:",url,"e:",e)
+      return get_error(e)
+    }
+  }, //async_getflags  
+
+
 }//Api
 
 
