42940b0cdc5be9031ab9bb3f87d3a1c86a333acb
ioedumac <eacevedof@yahoo.es>
2020-06-15 11:37:25 +0200

    vue: dots submenu backup

---
+++ vuejs/pannel/src/components/common/menus/submenu_rudc.vue
@@ -0,0 +1,72 @@
+<template>
+  <v-row>
+    <v-col cols="12" sm="6" offset-sm="3">
+      <v-card height="200px">
+        <v-card-title class="blue white--text">
+          <span class="headline">Menu</span>
+
+          <v-spacer></v-spacer>
+
+          <v-menu bottom left>
+            <template v-slot:activator="{ on, attrs }">
+              <v-btn
+                dark
+                icon
+                v-bind="attrs"
+                v-on="on"
+              >
+                <v-icon>mdi-dots-vertical</v-icon>
+              </v-btn>
+            </template>
+
+            <v-list>
+              <v-list-item
+                v-for="(item, i) in items"
+                :key="i"
+                @click="items.evt"
+              >
+                <v-list-item-title>{{ item.title }}</v-list-item-title>
+              </v-list-item>
+            </v-list>
+          </v-menu>
+        </v-card-title>
+
+        <v-card-text>Lorem Ipsum</v-card-text>
+      </v-card>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  name: "submenu-rudc",
+  data: () => ({
+    items: [
+      { title: 'Clone', evt:"clone" },
+      { title: 'Update', evt:"update" },
+      { title: 'Delete', evt:"remove"},
+      { title: 'Detail', evt:"detail" },
+    ],
+  }),
+
+  methods:{
+    clone(){
+      alert("clone")
+      this.$emit("evtclone")
+    },
+    update(){
+      alert("update")
+      this.$emit("evtupdate")
+    },
+    remove(){
+      alert("remove")
+      this.$emit("evtdelete")
+    },
+    detail(){
+      alert("detail")
+      this.$emit("evtdetail")
+    },
+  },
+
+}
+</script>
