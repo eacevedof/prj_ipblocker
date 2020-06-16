784cfea37dc005ae6b9dac5c923d4a17580cc15d
ioedumac <eacevedof@yahoo.es>
2020-06-16 21:34:09 +0200

    vue: cambio apiflag

--- vuejs/pannel/src/providers/apiflag.ts
+++ vuejs/pannel/src/providers/apiflag.ts
@@ -1,14 +1,41 @@
 import axios from "axios"
-import helpapify from "@/helpers/apify.ts"
-import db from "@/helpers/localdb"
 import {is_undefined, get_error} from "@/helpers/functions"
 
 let BASE_URL = "https://dbsapify.theframework.es"
 BASE_URL = "https://dbsapify.theframework.es"
 BASE_URL = "http://localhost:3000"
 
-const Api = {
-  
+const Apiflag = {
+ 
+    async_getflags: async(arrows) => {
+        if(is_undefined(arrows.length) || arrows.length==0)
+            return
+        
+            //https://restcountries.eu/rest/v2/alpha?codes=ES;PE
+        const url = `https://restcountries.eu/rest/v2/alpha?codes=`
+        //hay que enviar header: apify-auth: token
+        try {
+          const fnunique = (value, i, self) => (self.indexOf(value) == i)
+    
+          const arcodes = arrows.map(objrow => objrow.country).filter(fnunique)
+          if(!arcodes) return
+          const strcodes = arcodes.join(";")
+          const response = await axios.get(url+strcodes)
+          
+          //devuelve el num de registros afectados
+          if(is_undefined(response.data))
+            throw new Error("Wrong data received from server. getflags result")
+    
+          const fullinfo = response.data
+          const mindata = fullinfo.map(objinfo => ({name:objinfo.name,flag:objinfo.flag}))
+          //pr(mindata,"mindata")
+          return mindata
+        } 
+        catch (e) {
+          console.error("ERROR: apiflag.async_getflags.url:",url,"e:",e)
+          return get_error(e)
+        }
+      }, //async_getflags      
 }
 
-export default Api;
+export default Apiflag;
