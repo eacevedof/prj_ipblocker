a8a913cacccff3c414f660d8fec299ea727683f9
ioedumac <eacevedof@yahoo.es>
2020-06-17 22:06:08 +0200

    vue: ya devuelve los campos

--- vuejs/pannel/src/helpers/filter.ts
+++ vuejs/pannel/src/helpers/filter.ts
@@ -1,19 +1,37 @@
 import {pr, cl, is_defined} from "../helpers/functions"
 import { table } from '@/modules/iprequest/queries'
 const CS = "|" //command separator
-const LS = ":" //lable separator
+const LS = ":" //lable separator cmd:search |
 
 const mini = str => str.trim().toLowerCase()
 
 const get_field = (label, arconfig) => {
   const labelmin = mini(label)
-  const r = arconfig.map(obj => obj.table.fields.filter(objf => mini(objf.label) === labelmin) )
-  return r.filter(ar => ar.length>0)[0][0]
+  if(labelmin=="") return []
+  
+  const allfields = []
+  arconfig.forEach(objtable => {
+    
+    const alias = is_defined(objtable.table.alias) ? objtable.table.alias +".": ""
+
+    objtable.table.fields.forEach( objf => {
+      const fldlbl = mini(objf.label)
+      if(fldlbl !== labelmin) return
+      
+      const obj = {
+        field: `${alias}${objf.name}`,
+      }
+      allfields.push(obj)
+    })
+
+  })
+
+  return allfields
 }
 
 const filtercmd = (search, arconfig=[])=>{
   if(search.trim()=="") return []
-  //if()
+  if(!search.includes(LS)) return []
 
   const commands = search.split(CS).map(strcmd => mini(strcmd))
   
@@ -26,7 +44,6 @@ const filtercmd = (search, arconfig=[])=>{
   })
 
   //pr(arconfig,"arconfig"); return
-
   const r = labels.map(objcmd => get_field(objcmd.label, arconfig))
   cl(r,"r")
 
@@ -37,7 +54,7 @@ export const filtersimple = (search, arconfig) => {
   //const table = arconfig.table.name
   const allfields = []
   arconfig.forEach(objtable => {
-    const alias = is_defined(objtable.alias) ? objtable.alias +".": ""
+    const alias = is_defined(objtable.table.alias) ? objtable.table.alias +".": ""
     objtable.table.fields.forEach( objf => {
       const obj = {
         field: `${alias}${objf.name}`,
