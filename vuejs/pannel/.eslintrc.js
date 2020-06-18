8aa93cd6c1839943a9d6367d010c80eda2799259
ioedumac <eacevedof@yahoo.es>
2020-06-18 19:47:54 +0200

    vue: cambio parametros para ver si compila sin errores pero sin suerte

--- vuejs/pannel/.eslintrc.js
+++ vuejs/pannel/.eslintrc.js
@@ -5,15 +5,12 @@ module.exports = {
   },
   extends: [
     'plugin:vue/essential',
-    //'@vue/typescript/recommended',
+    '@vue/typescript/recommended',
   ],
   parserOptions: {
     ecmaVersion: 2020,
   },
-  rules: {
-
-    
-    
+  rules: {   
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
   },
