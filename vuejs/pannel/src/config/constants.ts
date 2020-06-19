34c900f536c98546435130e67163fa44bd715953
ioedumac <eacevedof@yahoo.es>
2020-06-19 12:29:55 +0200

    vue: cambio a schemainfo

--- vuejs/pannel/src/config/constants.ts
+++ vuejs/pannel/src/config/constants.ts
@@ -1,8 +1,9 @@
-//export const BASE_URL = "http://localhost:3000"
-//export const CONTEXT = "c3"
-//export const DB_NAME = "db_security"
+export const BASE_URL = "http://localhost:3000"
+export const CONTEXT = "c3"
+export const DB_NAME = "db_security"
 
-
+/*
 export const BASE_URL = "https://dbsapify.theframework.es"
 export const CONTEXT = "c3"
 export const DB_NAME = "dbs433062"
+*/
