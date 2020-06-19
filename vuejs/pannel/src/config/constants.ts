6ae421cd5727c0b093df43ceed52150ddc9d62f3
ioedumac <eacevedof@yahoo.es>
2020-06-19 12:43:06 +0200

    vue: cambio htaccess

--- vuejs/pannel/src/config/constants.ts
+++ vuejs/pannel/src/config/constants.ts
@@ -1,6 +1,6 @@
 export const BASE_URL = "http://localhost:3000"
 export const CONTEXT = "c3"
-export const DB_NAME = "db_security"
+export const DB_NAME = "db-security"
 
 /*
 export const BASE_URL = "https://dbsapify.theframework.es"
