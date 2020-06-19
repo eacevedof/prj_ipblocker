9ac5f96843131e88c6910c7afd0d7128908be8aa
ioedumac <eacevedof@yahoo.es>
2020-06-19 17:20:34 +0200

    vue: backup en ipblacklist

---
+++ vuejs/pannel/src/modules/ipblacklist/form_delete.vue
@@ -0,0 +1,167 @@
+<template>
+  <v-dialog v-model="is_visible" max-width="700px" persistent>
+    <v-card>    
+      <v-card-title class="red accent-4 white--text">
+        <span class="headline"> 
+          <v-icon color="white">mdi-trash-can-outline</v-icon> <b>Deleting IP Request!:</b> {{get_dialogtitle}}
+        </span>
+      </v-card-title>
+
+      <v-card-text>
+        <v-container>
+          <v-row v-if="error.title!='' || success.title!=''">
+            <v-col ms="12">
+              <notierror v-if="error.title!=''" :title="error.title"  :message="error.message" />
+              <notisuccess v-if="success.title!=''" :title="success.title"  :message="success.message" />
+            </v-col>
+          </v-row>
+          <v-row>
+            <v-col ms="5">
+              <v-text-field v-model="objrow.remote_ip" label="R. IP" disabled outlined dense />
+            </v-col>
+            <v-col ms="1">
+              <v-text-field v-model="objrow.country" disabled outlined dense label="Country" />
+            </v-col>
+            <v-col sm="6">
+              <v-text-field v-model="objrow.domain" disabled outlined dense label="Domain" />
+            </v-col>
+          </v-row>
+          <v-row>
+            <v-col sm="12">
+              <v-text-field v-model="objrow.whois" disabled outlined dense label="Whois" />
+            </v-col>                        
+          </v-row>
+          <v-row>
+            <v-col>  
+              <v-text-field v-model="objrow.request_uri" label="Uri" disabled outlined dense />
+              <v-textarea rows="1" v-model="objrow.get" label="GET" disabled outlined dense />
+            </v-col>
+            <v-col>
+              <v-textarea rows="1" v-model="objrow.post" label="POST" disabled outlined dense />
+              <v-text-field v-model="objrow.insert_date" disabled label="Date" outlined dense />
+            </v-col>
+          </v-row>
+          <progressbar :isvisible="issubmitting" />
+        </v-container>
+      </v-card-text>
+
+      <v-card-actions>
+        <v-spacer />
+        <v-btn color="blue-grey" :disabled="issubmitting" class="ma-2 white--text" @click="close">Close</v-btn>
+        <v-btn v-if="isaccept" color="red accent-4" :disabled="issubmitting" class="ma-2 white--text" @click="async_accept">Accept</v-btn>
+      </v-card-actions>
+
+    </v-card>
+  </v-dialog>
+</template>
+
+<script lang="ts">
+import apidb from "../../providers/apidb"
+import {get_obj_detete} from "../../modules/iprequest/queries"
+
+import progressbar from "@/components/common/bars/progress_bar.vue"
+import notisuccess from "@/components/common/notifications/notification_success.vue"
+import notierror from "@/components/common/notifications/notification_error.vue"
+
+export default {
+
+  name: "iprequest-formdelete",
+
+  components:{
+    progressbar,
+    notisuccess,
+    notierror,
+  },
+
+  props:{
+    //si se muestra el form
+    isvisible: Boolean,
+    objrow: {},
+  },
+
+  data: ()=>(
+    {
+      issubmitting: false,
+      error:{title:"",mesage:""},
+      success:{title:"",message:""},
+      isaccept: true,
+    }
+  ),
+
+  watch: {
+
+  },
+
+  //getters
+  computed:{
+    
+    get_dialogtitle(){
+      return `Nº:${this.objrow.id} - IP: ${this.objrow.remote_ip}`
+    },
+
+    is_visible:{
+      get(){
+        return this.isvisible
+      },
+      set(val){
+        //lanza un evento hacia afuera
+        this.$emit("evtclose",val)
+      }
+    },
+
+    is_submitting(){
+      return this.issubmitting
+    }
+
+  },
+  
+  //setters 
+  methods:{
+
+    set_error(title,message){
+      this.error.title = title
+      this.error.message = message
+    },
+
+    set_success(title,message){
+      this.success.title = title
+      this.success.message = message
+    },    
+
+    reset_alerts(){
+      this.set_error("","")
+      this.set_success("","")
+    },
+
+    close(){
+      this.reset_alerts()
+      this.is_visible = false
+      this.isaccept = true
+    },
+
+    async_accept: async function(){
+      this.reset_alerts()
+      this.issubmitting = true
+
+      const objparam={
+        fields: this.objrow,
+        keys:["id"]
+      }
+      
+      const objquery = get_obj_detete(objparam)
+      const result = await apidb.async_delete(objquery)
+      this.issubmitting = false
+      
+      if(result.error){  
+        this.set_error("Error",result.error)        
+        this.$emit("evtdelete","nok")
+        return
+      }
+      
+      this.set_success("Success",`Reults deleted ${result}`)
+      this.isaccept = false
+      this.$emit("evtdelete","ok")
+    },
+  },
+}
+</script>
---
+++ vuejs/pannel/src/modules/ipblacklist/index.vue
@@ -0,0 +1,368 @@
+<template>
+  <div>
+    <barover :isvisible="isfetching && !showform" />
+    <submenu :isvisible="issubmenu" :evtclick="evtsubmenu" v-on:evtselected="submenu_selected" v-on:evtclose="issubmenu=false" />
+
+    <detail v-if="crudopt=='detail'" :objrow="objrow" :isvisible="showform" v-on:evtclose="showform=false" />
+<!-- los hijos se comunican por eventos con los padres --
+    <forminsert v-if="crudopt=='insert'" :isvisible="showform" v-on:evtinsert="form_result" v-on:evtclose="showform=false" />
+    
+    <formclone v-if="crudopt=='clone'" :objrow="objrow" :isvisible="showform" v-on:evtclone="form_result" v-on:evtclose="showform=false" />
+    <formupdate v-if="crudopt=='update'" :objrow="objrow" :isvisible="showform" v-on:evtupdate="form_result" v-on:evtclose="showform=false" />
+    <formdelete v-if="crudopt=='delete'" :objrow="objrow" :isvisible="showform" v-on:evtdelete="form_result" v-on:evtclose="showform=false" />
+-->
+    <v-row
+      justify="center" align="center"
+    >
+      <v-col
+        cols="8"
+        md="8"
+      >
+        <v-text-field
+          ref="dbsearch"
+          v-model="dbsearch"
+          label="Search"
+          append-icon="mdi-shield-search"
+          outlined
+          hide-details="true"
+          color="secondary"
+        />
+        <sub>regs:{{page.foundrows}}</sub>
+      </v-col>
+    </v-row>
+    
+    <div v-if="page.ipages>1" class="text-center pt-2">
+      <v-pagination 
+        v-model="page.ipage" 
+        :page="page.ipage"
+        :length="page.ipages" 
+        @input="on_paginate"
+        total-visible="10"
+      />
+    </div>
+    
+    <v-data-table v-if="!isfetching" 
+      :headers="headers" 
+      :items="arrows"
+      :items-per-page="page.ippage"
+      hide-default-footer
+
+      :search="search" 
+
+      class="elevation-3"
+    >
+      <!-- inyecta en la cabecera de la tabla en la zona top-->
+      <template v-slot:top>
+        
+        <v-system-bar color="yellow lighten-1" />
+
+        <v-toolbar color="black darken-4">
+          
+          <v-btn
+            class="mx-2"
+            :elevation="10"
+            fab dark color="light-green accent-4"
+            @click="insert" 
+          ><v-icon>mdi-plus</v-icon></v-btn>
+
+          <v-divider class="mx-4" inset vertical/>
+          <v-toolbar-title class="yellow--text font-weight-bold">
+            IP Request
+          </v-toolbar-title>
+        </v-toolbar>
+
+        <!-- barra busqueda -->
+        <v-col>
+          <v-text-field v-model="search" append-icon="search" label="Search in page result" single-line hide-details />
+        </v-col>
+
+      </template>
+      <!-- fin cabecer tabla -->
+
+      <!-- botones columna -->
+      <template v-slot:item.colbuttons="{ item }">
+        <v-btn class="m4-2" fab dark small color="cyan" @click="submenu(item)">
+          <v-icon dark>mdi-dots-vertical</v-icon>
+        </v-btn>
+      </template>
+    </v-data-table>
+
+    <notifsnack :showsnack="snack.isvisible" :innertext="snack.innertext" v-on:evtclose="snack.isvisible=false" />
+    
+    <div v-if="page.ipages>1" class="text-center pt-2">
+      <v-pagination 
+        v-model="page.ipage" 
+        :page="page.ipage"
+        :length="page.ipages" 
+        @input="on_paginate"
+        total-visible="10"
+      />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import {mapMutations, mapActions, mapState} from "vuex"
+import {pr} from "../../helpers/functions"
+import apidb from "../../providers/apidb"
+import url from "../../helpers/url"
+import get_filters from "../../helpers/filter"
+import {get_obj_list, config, grid} from "../../modules/iprequest/queries"
+
+
+import notifsnack from "@/components/common/notifications/notification_snackbar.vue"
+import barover from "@/components/common/bars/progress_barover.vue"
+import submenu from "@/components/common/menus/submenu_rudc.vue"
+import detail from "@/modules/iprequest/detail.vue"
+//import forminsert from "@/modules/iprequest/form_insert.vue"
+//import formupdate from "@/modules/iprequest/form_update.vue"
+//import formdelete from "@/modules/iprequest/form_delete.vue"
+//import formclone from "@/modules/iprequest/form_clone.vue"
+
+export default {
+  name: "iprequest-index",
+  
+  components: {
+    notifsnack,
+    submenu,
+    //forminsert,
+    detail,
+    //formupdate,
+    //formdelete,
+    //formclone,
+    barover,
+  },
+
+  data: () => ({
+
+    issubmenu: false,
+    evtsubmenu: {x:0,y:0},
+
+    isfetching: false,
+    showform: false,
+    crudopt: "",
+
+    search: "",
+    
+    objrow: {},
+
+    headers: grid.headers,
+    arrows: [],
+
+    snack:{
+      isvisible:false,
+      innertext: "",
+    },
+
+    page:{
+      ipage: 1,     //pagina actual
+      ippage: 50,  //regs por pag
+      ipages: 0,    //num de paginas
+      foundrows:0,  //total de registros
+    },
+
+    dbsearch:"",
+
+  }),//data
+
+  watch:{
+    dbsearch(val) {
+      if (!val) {
+        return;
+      }
+
+      this.on_dbsearch(val);
+    }
+  },
+
+  //no puede ser asincrono pq el ciclo de vida no aplica await
+  beforeMount(){
+    console.log("iprequest.index.beforeMount")
+  },
+
+
+  //lo marco como async pq tengo que resolver peticiones
+  mounted: async function(){
+
+    console.log("iprequest.index.mounted.islogged antes de llamar a async_islogged",this.islogged)
+    //async_islogged comprueba el token en local si existe comprueba si es válido
+    //si pasa estos filtros this.islogged tendría el valor true
+    const response = await this.async_islogged()
+    console.log("iprequest.index.mounted.islogged",this.islogged)
+
+    if(!this.islogged){
+      this.$router.push({name:"login"})
+      return
+    }
+
+    //error network
+    if(response.error){
+      this.$emit("evterror",response.error)
+      return 
+    }
+
+    console.log("iprequest.index.mounted islogged:",this.islogged)
+    const objpage = this.get_page()
+
+    this.page.ipage = objpage.ipage
+    const objparam = {
+      page: {...objpage},
+      filters: {}
+    }
+
+    //pr(objparam,"objparam in async")
+    await this.async_loaddata(objparam)
+  },
+
+  computed:{
+    ...mapState(["islogged"]),
+  },
+
+  methods:{
+    //setters
+    ...mapActions(["async_islogged"]),
+
+    get_page(){
+      const ipage = parseInt(url.get_param("page",this.$route)) || 1
+      const ippage = this.page.ippage
+      const ifrom = (ipage-1) * ippage
+
+      return {
+        ipage: ipage,
+        ippage: ippage,
+        ifrom: ifrom,
+      }
+    },
+//////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////
+    async_loaddata: async function(objparam={page:{},filters:{}}){
+      this.isfetching = true
+      const objquery = get_obj_list(objparam)
+      const response = await apidb.async_get_list(objquery)
+
+      this.arrows = response.result
+      this.page.ipage = objparam.page.ipage
+      this.page.foundrows = response.foundrows
+      this.page.ipages = Math.ceil(this.page.foundrows/objparam.page.ippage)
+
+      console.table("iprequest.index.async_loaddata.page.foundrows:",this.page.foundrows)
+      this.isfetching = false
+
+      //si hay un formulario en pantalla no se le aplica el foco a esto
+      if(!this.showform)
+        this.$refs.dbsearch.focus()
+
+    }, //async_loaddata
+//////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////
+    on_dbsearch(text) {
+      // cancel pending call
+      clearTimeout(this.debounceid);
+
+      this.issearching = true;
+      // delay new call 500ms
+      this.debounceid = setTimeout(() => {
+        text = text.trim()
+        if(this.$route.path !== `/ip-request/1`){
+          this.$router.push({ name: 'iprequest', params: { page: 1 } })      
+        }
+      
+        const objpage = this.get_page()
+
+        this.page.ipage = objpage.ipage
+        const objparam = {
+          page: {...objpage},
+          filters: {}
+        }
+
+        if(text!==""){
+          objparam.filters = get_filters(text, config)
+          //pr(objparam,"objparam")
+          this.async_loaddata(objparam)
+        }
+        else
+          this.async_loaddata(objparam)
+        
+        this.issearching = false
+      }, 1000);
+
+    },   
+
+    on_paginate(ipage){
+      if(this.$route.path !== `/ip-request/${ipage}`)
+        this.$router.push({ name: 'iprequest', params: { page: ipage } })
+
+      const objpage = this.get_page()
+      //pr(objpage,`ipage: ${ipage}`)
+
+      this.page.ipage = objpage.ipage
+      const objparam = {
+        page: {...objpage},
+        filters: {}
+      }      
+      if(this.dbsearch)
+        objparam.filters = get_filters(this.dbsearch, config)
+
+      this.async_loaddata(objparam)
+    },
+
+    show_form(){
+      this.showform = true
+    },
+
+    form_result(val){
+      const objpage = this.get_page()
+      //pr(objpage,`ipage: ${ipage}`)
+
+      this.page.ipage = objpage.ipage
+      const objparam = {
+        page: {...objpage},
+        filters: {}
+      }      
+      if(this.dbsearch)
+        objparam.filters = get_filters(this.dbsearch, config)
+
+      this.async_loaddata(objparam)
+    },
+
+    insert(){
+      this.crudopt = "insert"
+      this.show_form()
+    },
+
+    update(objrow){
+      this.crudopt = "update"
+      this.objrow = objrow
+      this.show_form()
+    },
+
+    clone(objrow){
+      this.crudopt = "clone"
+      this.objrow = objrow
+      this.show_form()
+    },    
+
+    remove(objrow){
+      this.crudopt = "delete"
+      this.objrow = objrow
+      this.show_form()
+    },
+
+    submenu(objrow){
+      const e = window.event
+      this.objrow = objrow
+      this.issubmenu = true
+      this.evtsubmenu = e
+    },
+
+    submenu_selected(option){
+      this.crudopt = option
+      this.show_form()
+    },
+    
+  }//methods  
+
+};
+</script>
