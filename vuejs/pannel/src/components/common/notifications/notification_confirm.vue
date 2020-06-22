78d0dc6ac031de0145d1e1d9dc31c88813805342
ioedumac <eacevedof@yahoo.es>
2020-06-22 13:29:34 +0200

    vue: creo notif-confirm

---
+++ vuejs/pannel/src/components/common/notifications/notification_confirm.vue
@@ -0,0 +1,62 @@
+<template>
+<v-row justify="center">
+  <v-dialog
+    v-model="is_visible"
+    max-width="290"
+  >
+    <v-card>
+      <v-card-title class="headline" v-if="title!=''">{{title}}</v-card-title>
+
+      <v-card-text v-if="message!=''">
+        {{ message }}
+      </v-card-text>
+
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <v-btn
+          color="yellow darken-1"
+          text
+          @click="action('cancel')"
+        >
+          Disagree
+        </v-btn>
+
+        <v-btn
+          color="black darken-1"
+          text
+          @click="action('ok')"
+        >
+          Agree
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+
+  </v-dialog>
+</v-row>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'notificationconfirm',
+  props: {
+    isvisible: Boolean,
+    title: String,
+    message: String
+  },
+ computed:{
+    is_visible:{
+      get(){
+        return this.isvisible
+      },
+      set(v){
+        this.$emit("evtclose",v)
+      }
+    }
+  },
+  methods:{
+    action(type){
+      this.$emit("evtaction",type)
+    }
+  }
+}
+</script>
