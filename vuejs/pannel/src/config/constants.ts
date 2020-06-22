722590763ad2c29830caa1a3edd1db724469394e
ioedumac <eacevedof@yahoo.es>
2020-06-22 15:55:41 +0200

    vue: version v1.0.0

--- vuejs/pannel/src/config/constants.ts
+++ vuejs/pannel/src/config/constants.ts
@@ -1,12 +1,12 @@
-export const APP_VERSION = "1.0.0.Beta.1"
+export const APP_VERSION = "v1.0.0"
 
-/**/
+/**
 export const BASE_URL = "http://localhost:3000"
 export const CONTEXT = "c3"
 export const DB_NAME = "db-security"
 /**/
 
-/**
+/**/
 export const BASE_URL = "https://dbsapify.theframework.es"
 export const CONTEXT = "c3"
 export const DB_NAME = "db-security"
