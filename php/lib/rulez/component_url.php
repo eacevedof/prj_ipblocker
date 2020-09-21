2e88dcff54d00159c055694000c0224c5a60f732
ioedumac <eacevedof@yahoo.es>
2020-09-21 21:49:59 +0200

    component_url

--- php/lib/rulez/component_url.php
+++ php/lib/rulez/component_url.php
@@ -2,10 +2,24 @@
 namespace TheFramework\Rulez;
 use TheFramework\Rulez;
 
+use TheFramework\Components\ComponentString;
+use TheFramework\Components\ComponentArray;
+
 class ComponentUrl implements IRulez
 {
+
+    private string $requrl;
+    private array $arrulez;
+
+    public function __construct(string $requrl, array $arrulez)
+    {
+        $this->requrl = new ComponentString($requrl);
+        $this->arrulez = new ComponentArray($arrulez);
+    }
+
     public function get()
     {
+
         // TODO: Implement validate() method.
     }
 }
