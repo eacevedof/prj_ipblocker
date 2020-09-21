1ef4e8e20f304c8f039ad492ef16f5476ff46975
ioedumac <eacevedof@yahoo.es>
2020-09-21 19:37:47 +0200

    backup rulez

---
+++ php/lib/rulez/component_url.php
@@ -0,0 +1,11 @@
+<?php
+namespace TheFramework\Rulez;
+use TheFramework\Rulez;
+
+class ComponentUrl implements IRulez
+{
+    public function get()
+    {
+        // TODO: Implement validate() method.
+    }
+}
