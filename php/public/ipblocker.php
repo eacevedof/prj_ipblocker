35f4807f01bd8c297fba550ad72cf6b543475554
ioedumac <eacevedof@yahoo.es>
2020-09-22 14:03:06 +0200

    reorganizando carpetas

--- php/public/ipblocker.php
+++ php/public/ipblocker.php
@@ -1,7 +1,9 @@
 <?php
 //ipblocker.php
-$pathboot = realpath(__DIR__ . "/../boot");
-include("$pathboot/appbootstrap.php");
+require __DIR__.'/../vendor/autoload.php';
+print_r(get_included_files());
+//$pathboot = realpath(__DIR__ . "/../boot");
+//include("$pathboot/appbootstrap.php");
 
-use \TheFramework\Components\ComponentIpblocker;
-(new ComponentIpblocker())->handle_request();
+//use \TheFramework\Components\ComponentIpblocker;
+//(new ComponentIpblocker())->handle_request();
