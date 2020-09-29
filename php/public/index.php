f11ba58959e669eda4c532656958bd7f86a285c1
ioedumac <eacevedof@yahoo.es>
2020-09-29 13:21:14 +0200

    mejoro public/index y uso pp(get_included_files) 18

--- php/public/index.php
+++ php/public/index.php
@@ -4,13 +4,7 @@ require __DIR__."/../vendor/autoload.php";
 use function Ipblocker\Functions\pp;
 use Ipblocker\Controllers\ControllerMain;
 
-//pp(get_included_files());
 if(defined("IPB_DOTRACK") && IPB_DOTRACK) {
     (new ControllerMain)->handle_request();
 }
-
-//$pathboot = realpath(__DIR__ . "/../boot");
-//include("$pathboot/appbootstrap.php");
-
-//use \Ipblocker\Component\ComponentIpblocker;
-//(new ComponentIpblocker())->handle_request();
+pp(get_included_files(),"index.php get_included_files");
--- php/src/Controllers/ControllerMain.php
+++ php/src/Controllers/ControllerMain.php
@@ -74,7 +74,7 @@ class ControllerMain
 
     public function handle_request()
     {
-        if($this->_is_ipuntracked()) return;
+        //if($this->_is_ipuntracked()) return;
         $this->prov->save_request();
         if($this->_is_search_bot()) return;
         //guarda en blacklist si detecta contenido prohibido y si no existiera en bl
