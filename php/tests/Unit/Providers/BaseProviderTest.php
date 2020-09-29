2a6ea3862c2671e26e6a5f68264492b9266fbdf2
ioedumac <eacevedof@yahoo.es>
2020-09-29 17:23:44 +0200

    test_isnot_blacklisted

--- php/tests/Unit/Providers/BaseProviderTest.php
+++ php/tests/Unit/Providers/BaseProviderTest.php
@@ -8,6 +8,7 @@ class BaseProviderTest extends BaseTest
 {
     private const IP_UNATRACKED = "127.0.0.U";
     private const IP_BLACKLISTED = "127.0.0.B";
+    private const IP_NOTBLACKLISTED = "127.0.0.NB";
 
     private function _add_untracked()
     {
@@ -68,7 +69,7 @@ class BaseProviderTest extends BaseTest
     {
         $this->_reset_fullrequest();
         $this->_add_blacklist();
-        $_SERVER["REMOTE_ADDR"] = self::IP_UNATRACKED;
+        $_SERVER["REMOTE_ADDR"] = self::IP_NOTBLACKLISTED;
         $prov = new BaseProvider();
         $r = $prov->is_blacklisted();
         $this->assertEmpty($r);
