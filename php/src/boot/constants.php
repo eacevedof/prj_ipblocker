adc5531962d43aa22364c96240f6fed25cf557ea
ioedumac <eacevedof@yahoo.es>
2020-09-29 15:18:01 +0200

    constants.php

--- php/src/boot/constants.php
+++ php/src/boot/constants.php
@@ -1,9 +1,13 @@
 <?php
 //constants.php
-define("IPB_ENABLE_LOGS",1);
-define("IPB_DOTRACK",1);
 $pathconfig = realpath(__DIR__ . "/../../../config");
 $pathlogs = realpath(__DIR__ . "/../../../logs");
 //echo is_dir($pathlogs);die($pathlogs);
 define("IPB_PATH_CONFIG",$pathconfig);
 define("IPB_PATH_LOGS",$pathlogs);
+
+//=========================
+//        FLAGS
+//=========================
+define("IPB_ENABLE_LOGS",1);
+define("IPB_DOTRACK",1);
