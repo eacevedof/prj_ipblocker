1cef811b71a1c5e6d2860a5a3781b7e996797272
ioedumac <eacevedof@yahoo.es>
2020-10-03 17:55:09 +0200

    test_connection

--- backend_web/tests/Unit/Components/Db/MysqlComponentTest.php
+++ backend_web/tests/Unit/Components/Db/MysqlComponentTest.php
@@ -3,30 +3,21 @@
 namespace Tests\Unit\Components\Db;
 
 use Tests\Unit\BaseTest;
-use Ipblocker\Components\Db\MysqlComponent;
 
 class MysqlComponentTest extends BaseTest
 {
     public function test_connection()
     {
-        $arconf = [
-            "server"    => "127.0.0.1",
-            "database"  => "db_ipblocker",
-            "port"      => "3306",
-            "user"      => "root",
-            "password"  => "1234",
-        ];
-
-        $o = new MysqlComponent($arconf);
+        $db = $this->_get_db();
         $sql = "
         SELECT COUNT(*)
         FROM information_schema.tables 
         WHERE table_schema =  DATABASE()
         AND table_name =  'app_ip';
         ";
-        $r = $o->query($sql);
-        if($o->is_error()) $this->logd($o->get_errors(),"test_connection.errors");
-        $this->assertEquals(false,$o->is_error());
+        $r = $db->query($sql);
+        if($db->is_error()) $this->logd($db->get_errors(),"test_connection.errors");
+        $this->assertEquals(false,$db->is_error());
     }
 
 }
