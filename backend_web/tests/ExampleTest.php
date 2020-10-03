2a498e6fb39b376841399c6fe3adf535a0606899
ioedumac <eacevedof@yahoo.es>
2020-10-03 22:37:11 +0200

    example test

---
+++ backend_web/tests/ExampleTest.php
@@ -0,0 +1,14 @@
+<?php
+namespace Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class ExampleTest extends TestCase
+{
+    public function test_demo()
+    {
+        $r = true;
+        $this->assertTrue($r);
+    }
+
+}//ExampleTest
