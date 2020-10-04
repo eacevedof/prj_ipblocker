6e92487afefd2c7f4b6e9391d6286b51b5be9da9
ioedumac <eacevedof@yahoo.es>
2020-10-04 13:37:58 +0200

    index.php a

--- backend_web/public/index.php
+++ backend_web/public/index.php
@@ -5,6 +5,6 @@ require __DIR__."/../vendor/autoload.php";
 use Ipblocker\Controllers\ControllerMain;
 
 if(defined("IPB_DOTRACK") && IPB_DOTRACK) {
-    $ipb_f = (new ControllerMain); $ipb_f(); unset($ipb_f);
+    $ipbfn = (new ControllerMain); $ipbfn(); unset($ipbfn);
 }
 //pp(get_included_files(),"index.php get_included_files");
